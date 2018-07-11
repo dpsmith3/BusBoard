25c807a806b970c803b76b7f14e878df6ef4dc97
Daniel Smith <Daniel.Smith@softwire.com>
2018-07-11 11:56:20 +0100

    BusBoard displays the next five buses at a stop

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const request = require('request');
 const readline = require('readline-sync');
 
 function getBusStopCode() {
-    console.log("Please enter a bus stop code");
+    console.log("Please enter a bus stop code (eg. 490008660N)");
     return readline.prompt();
 }
-const busStopCode = '490008660N';
+
+function getMinsAndSeconds(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = time - minutes * 60;
+    return `${minutes} minutes and ${seconds} seconds.`
+}
 
 function getCurrentStopData(busStopCode) {
     const url = `https://api.tfl.gov.uk/StopPoint/${busStopCode}/Arrivals?app_id=fc74bda1&app_key=e49db77088e0f0288fcb620712571331`;
@@ -18,19 +23,26 @@ function getCurrentStopData(busStopCode) {
         } else {
 
             // 2. Extract data from body
-            const data = JSON.parse(body);
+            const rawData = JSON.parse(body);
+
+            const sortedData = rawData.sort(function compare(a, b) {
+                if (a.timeToStation < b.timeToStation) {
+                    return -1;
+                }
+                if (a.timeToStation > b.timeToStation) {
+                    return 1;
+                }
+                return 0;
+            });
 
             // 3. Display it
-            for (i = 1; i <= 5; i ++) {
-                console.log(`${i}: ${data[i].lineId} to ${data[i].destinationName}, in ${data[i].timeToStation} seconds`);
+            for (let bus of sortedData.slice(0, 5)) {
+                console.log(`The next five buses will be: ${sortedData.indexOf(bus) + 1}: ${bus.lineId} to ${bus.destinationName}, in ${getMinsAndSeconds(bus.timeToStation)}`);
             }
         }
     });
-
 }
 
-
 //Main
+const busStopCode = getBusStopCode()
 getCurrentStopData(busStopCode);
-
-console.log("CHANGED THIS");
